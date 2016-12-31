b8552282b82b8343ed93eb29005f461307cfb29c
François Poirotte <clicky@erebot.net>
2016-12-31 19:46:06 +0100

    Update RTD conf

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+from os.path import join
 import sys
 import glob
 import shutil
@@ -17,7 +18,7 @@ except ImportError:
 def prepare(globs, locs):
     # Where are we?
     cwd = os.getcwd()
-    root = os.path.abspath(os.path.join(cwd, '..', '..'))
+    root = os.path.abspath(join(cwd, '..', '..'))
 
     git = Popen('which git 2> %s' % os.devnull, shell=True,
                 stdout=PIPE).stdout.read().strip()
@@ -26,8 +27,7 @@ def prepare(globs, locs):
 
     locs['rtd_slug'] = os.path.basename(os.path.dirname(os.path.dirname(root)))
     locs['rtd_version'] = os.path.basename(root)
-    pybabel = os.path.join(root, '..', '..', 'envs',
-                           locs['rtd_version'], 'bin', 'pybabel')
+    pybabel = join(root, '..', '..', 'envs', locs['rtd_version'], 'bin', 'pybabel')
 
     print "git version:"
     call([git, '--version'])
@@ -50,7 +50,7 @@ def prepare(globs, locs):
                     stdout=PIPE).communicate()[0].strip()
     git_hash = Popen([git, 'rev-parse', 'HEAD'],
                     stdout=PIPE).communicate()[0].strip()
-    project = origin.rpartition('/')[2]
+    vendor, project = origin.rpartition(':')[2].split('/')[-2:]
     if project.endswith('.git'):
         project = project[:-4]
     os.environ['SPHINX_PROJECT'] = project
@@ -65,29 +65,26 @@ def prepare(globs, locs):
         locs['tags'].add('devel')
 
     # Clone or update dependencies
-    buildenv = os.path.join(root, 'vendor', 'erebot', 'buildenv')
-    natives = os.path.join(root, 'vendor', 'fpoirotte', 'natives4doxygen')
-    generic_doc = os.path.join(root, 'docs', 'src', 'generic')
     for repository, path in (
-        ('git://github.com/Erebot/Buildenv.git', buildenv),
-        ('git://github.com/fpoirotte/PHPNatives4Doxygen', natives),
-        ('git://github.com/Erebot/GenericDoc.git', generic_doc),
+        ('git://github.com/Erebot/Buildenv.git', join(root, 'vendor', 'erebot', 'buildenv')),
+        ('git://github.com/fpoirotte/PHPNatives4Doxygen', join(root, 'vendor', 'fpoirotte', 'natives4doxygen')),
+        ('git://github.com/Erebot/GenericDoc.git', join(root, 'docs', 'src', 'generic')),
     ):
         if not os.path.isdir(path):
             os.makedirs(path)
             print "Cloning %s into %s..." % (repository, path)
             call([git, 'clone', repository, path])
-        elif os.path.isdir(os.path.join(path, '.git')):
+        elif os.path.isdir(join(path, '.git')):
             os.chdir(path)
             print "Updating clone of %s in %s..." % (repository, path)
             call([git, 'checkout', 'master'])
             call([git, 'pull'])
             os.chdir(root)
 
-    composer = json.load(open(os.path.join(root, 'composer.json'), 'r'))
+    composer = json.load(open(join(root, 'composer.json'), 'r'))
 
     # Run doxygen
-    call([doxygen, os.path.join(root, 'Doxyfile')], env={
+    call([doxygen, join(root, 'Doxyfile')], env={
         'COMPONENT_NAME': os.environ['SPHINX_PROJECT'],
         'COMPONENT_VERSION': os.environ['SPHINX_VERSION'],
         'COMPONENT_BRIEF': composer.get('description', ''),
@@ -95,48 +92,55 @@ def prepare(globs, locs):
 
     # Remove extra files/folders.
     try:
-        shutil.rmtree(os.path.join(root, 'build'))
-    except OSError:
-        pass
-    os.mkdir(os.path.join(root, 'build'))
+        print "Removing %s ..." % join(root, 'build')
+        shutil.rmtree(join(root, 'build'))
+    except OSError, e:
+        print "Could not delete build folder recursively"
+        print "Error was: %s" % str(e)
+    os.mkdir(join(root, 'build'))
     shutil.move(
-        os.path.join(root, 'docs', 'api', 'html'),
-        os.path.join(root, 'build', 'apidoc'),
+        join(root, 'docs', 'api', 'html'),
+        join(root, 'build', 'apidoc'),
     )
     try:
+        print "Moving %s to %s ..." % (
+            join(root, '%s.tagfile.xml' % os.environ['SPHINX_PROJECT']),
+            join(root, 'build', 'apidoc', '%s.tagfile.xml' % os.environ['SPHINX_PROJECT']),
+        )
         shutil.move(
-            os.path.join(root, '%s.tagfile.xml' %
-                os.environ['SPHINX_PROJECT']),
-            os.path.join(root, 'build', 'apidoc', '%s.tagfile.xml' %
-                os.environ['SPHINX_PROJECT'])
+            join(root, '%s.tagfile.xml' % os.environ['SPHINX_PROJECT']),
+            join(root, 'build', 'apidoc', '%s.tagfile.xml' % os.environ['SPHINX_PROJECT'])
         )
     except OSError:
-        pass
+        print "Could not move the tagfile to its final destination"
+        print "Error was: %s" % str(e)
 
     # Copy translations for generic docs to catalogs folder.
-    gen_i18n = os.path.join(root, 'docs', 'src', 'generic', 'i18n', '.')[:-1]
-    for translation in glob.iglob(os.path.join(gen_i18n, '*')):
-        target_dir = os.path.join(
+    gen_i18n = join(root, 'docs', 'src', 'generic', 'i18n', '.')[:-1]
+    for translation in glob.iglob(join(gen_i18n, '*')):
+        target_dir = join(
             root, 'docs', 'i18n',
             translation[len(gen_i18n):],
             'LC_MESSAGES', 'generic'
         )
-        translation = os.path.join(translation, 'LC_MESSAGES', 'generic')
+        translation = join(translation, 'LC_MESSAGES', 'generic')
         shutil.rmtree(target_dir, ignore_errors=True)
+        print "Copying %s/ to %s/ ..." % (translation, target_dir)
         shutil.copytree(translation, target_dir)
 
     # Compile translation catalogs.
-    for locale_dir in glob.iglob(os.path.join(root, 'docs', 'i18n', '*')):
+    for locale_dir in glob.iglob(join(root, 'docs', 'i18n', '*')):
         for base, dirnames, filenames in os.walk(locale_dir):
             for po in fnmatch.filter(filenames, '*.po'):
-                po = os.path.join(base, po)
+                po = join(base, po)
                 mo = po[:-3] + '.mo'
+                print "Compiling %s into %s ..." % (po, mo)
                 call([pybabel, 'compile', '-f', '--statistics',
                       '-i', po, '-o', mo])
 
     # Load the real Sphinx configuration file.
     os.chdir(cwd)
-    real_conf = os.path.join(buildenv, 'sphinx', 'conf.py')
+    real_conf = join(root, 'vendor', 'erebot', 'buildenv', 'sphinx', 'conf.py')
     print "Including real configuration file (%s)..." % (real_conf, )
     execfile(real_conf, globs, locs)
 
@@ -144,30 +148,19 @@ def prepare(globs, locs):
     # - Theme
     if 'html_extra_path' not in locs:
         locs['html_extra_path'] = []
-    locs['html_extra_path'].append(os.path.join(root, 'build'))
-    locs['html_theme'] = 'haiku'
+    locs['html_extra_path'].append(join(root, 'build'))
 
     # - I18N
     if 'locale_dirs' not in locs:
         locs['locale_dirs'] = []
-    locs['locale_dirs'].insert(0, os.path.join(root, 'docs', 'i18n'))
+    locs['locale_dirs'].insert(0, join(root, 'docs', 'i18n'))
 
     if 'rst_prolog' not in locs:
         locs['rst_prolog'] = ''
-    locs['rst_prolog'] += '\n    .. _`this_commit`: https://github.com/%s/commit/%s\n' % (
+    locs['rst_prolog'] += '\n    .. _`this_commit`: https://github.com/%s/%s/commit/%s\n' % (
+        vendor,
         project,
         git_hash,
     )
-
-    # - Custom roles
-    if 'doxylinks' in locs and 'api' in locs['doxylinks']:
-        locs['doxylinks']['api'] = (
-            locs['doxylinks']['api'][0],
-            'file://%s' % urllib.quote(
-                os.path.join(root, 'build', 'apidoc', '%s.tagfile.xml' %
-                    os.environ['SPHINX_PROJECT'])
-            )
-        )
-
 
 prepare(globals(), locals())
